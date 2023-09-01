017ab03684e6a75a97dabd735a7be2ed8961cb7c
Ronny Pacheco <ronny.pach@gmail.com>
2023-08-31 23:56:35 -0400

    c3

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from changes in deploy</h1>
+        <h1>V3</h1>
       </header>
     </div>
   );
